a0d01cf82fb69f302b86618ca83d4e9749f6dec4
Tam <tam.info.math@gmail.com>
2014-06-01 23:12:15 +0900

    recipe 07.

--- BTChat/src/com/example/btchat/MainService.java
+++ BTChat/src/com/example/btchat/MainService.java
@@ -1,6 +1,9 @@
 package com.example.btchat;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.util.UUID;
 
@@ -9,8 +12,10 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
 import android.os.RemoteException;
+import android.widget.Toast;
 
 public class MainService extends Service implements Runnable {
 	private BluetoothSocket socket = null;
@@ -39,6 +44,18 @@ public class MainService extends Service implements Runnable {
 		new Thread(this).start();
 	}
 
+	private Handler handler = new Handler();
+
+	private void update(final String message) {
+		this.handler.post(new Runnable() {
+			@Override
+			public void run() {
+				Toast.makeText(MainService.this, message, Toast.LENGTH_LONG)
+						.show();
+			}
+		});
+	}
+
 	@Override
 	public void run() {
 		try {
@@ -50,6 +67,16 @@ public class MainService extends Service implements Runnable {
 			this.socket = device
 					.createInsecureRfcommSocketToServiceRecord(uuid);
 			this.socket.connect();
+			InputStream is = this.socket.getInputStream();
+			BufferedReader reader = new BufferedReader(
+					new InputStreamReader(is));
+			while (true) {
+				String line = reader.readLine();
+				if (line == null) {
+					break;
+				}
+				update(line);
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
